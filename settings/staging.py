15300365d36ebbe7be59f850447e7a9fb97453df
meganduffy <meganemilyduffy@gmail.com>
2017-04-01 16:37:47 +0100

    add engine and name to databases

--- settings/staging.py
+++ settings/staging.py
@@ -12,10 +12,10 @@ DATABASES = {
 
 DATABASES['default'] = dj_database_url.config(
     "mysql://be7e855bf7d62c:66cc4b48@eu-cdbr-west-01.cleardb.com/heroku_f373066c6c1d6bc")
-#
-# DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
-#
-# DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
+
+DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+
+DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
 #
 # CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
 #
