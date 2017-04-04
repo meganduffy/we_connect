5f75c54136771f3596147d10b8aeef1f16204da2
meganduffy <meganemilyduffy@gmail.com>
2017-04-04 13:05:52 +0100

    add databases engine

--- settings/staging.py
+++ settings/staging.py
@@ -16,10 +16,7 @@ DATABASES['default'] = dj_database_url.config(
 DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 DATABASES['default']['NAME'] = os.path.join(BASE_DIR, 'db.sqlite3')
-#
-# CLEAR_DB_URL = os.environ.get("CLEARDB_DATABASE_URL", "")
-#
-# DATABASES['default'] = dj_database_url.parse(CLEAR_DB_URL)
+
 
 ALLOWED_HOSTS = ['we-connect.herokuapp.com', 'fb2eea7a.ngrok.io']
 
